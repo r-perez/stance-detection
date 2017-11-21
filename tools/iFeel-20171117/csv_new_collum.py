c18f5941988680f10a5ed352fda48f2e9fcb59a6
Rafael Perez <rafael.perez@dcc.ufmg.br>
2017-11-21 10:26:20 -0200

    iFeel script que conta colunas gerado, e com comentarios

--- tools/iFeel-20171117/csv_new_collum.py
+++ tools/iFeel-20171117/csv_new_collum.py
@@ -9,16 +9,21 @@ def most_common(lst):
 
 # abre um arquivo de leitura csv, conta a ocorrencia do mais comum da lista e salva em um novo arquivo
 def read_csv_join_write_new_csv_with_collum(src, src_out):
+    # abre o arquivo de entrada
     with open(src, 'rb') as csvfile:
         fin = csv.reader(csvfile)
+        # abre o arquivo de saida
         with open(src_out, 'wb') as csvfile:
             fout = csv.writer(csvfile)
             for row in fin:
+                # escreve na saida uma linha com o primeiro elemento da linha da entrada,
+                # e com a contagem da maior ocorrencia das outras colunas da linha, exceto da primeira coluna
                 fout.writerow([row[0], most_common(row[1:len(row)])])
 
 def main():
     filename = 'lula_df_texto_'
     extension = '.csv'
+    # itera pelas opcoes contra e favor
     for i in ['contra', 'favor']:
         src = 'iFeel-{0}{1}.xlsx{2}'.format(filename, i, extension)
         src_out = 'AnSen-{0}{1}{2}'.format(filename, i, extension)
